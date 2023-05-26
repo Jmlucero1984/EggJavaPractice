9546cd0dc612fc7f20de58f5186780e8a31e510d
Jmlucero1984 <jmlucero1984@gmail.com>
2023-05-26 19:38:59 -0300

    Create Overloaded Class Constructors

--- src/main/java/com/jml/eggexercises/Exercise.java
+++ src/main/java/com/jml/eggexercises/Exercise.java
@@ -1,4 +1,3 @@
-
 package com.jml.eggexercises;
 
 import java.awt.event.ActionEvent;
@@ -13,57 +12,54 @@ import javax.swing.text.SimpleAttributeSet;
 
 /* @author jmlucero */
 public abstract class Exercise {
+
     public JTextPane jTextPane;
     public JTextField jTextField;
     public Document doc;
     public java.awt.event.ActionListener myListener;
-    
-   public Exercise(JTextPane jTextPane, JTextField jTextField,Document doc) {
+
+    public Exercise(JTextPane jTextPane, JTextField jTextField, Document doc) {
         this.jTextPane = jTextPane;
-        this.jTextField= jTextField;
-        this.doc= doc;
-        myListener= new java.awt.event.ActionListener() {
+        this.jTextField = jTextField;
+        this.doc = doc;
+        myListener = new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 action(evt);
             }
         };
         jTextField.addActionListener(myListener);
-     
+
     }
 
-   
-   
-   
-   
- 
     protected void action(ActionEvent evt) {
-       
+
     }
-    
+
     protected void removeListener() {
         jTextField.removeActionListener(myListener);
     }
 
-    protected void AddTextToPane(String args, SimpleAttributeSet set){
+    protected void AddTextToPane(String args, SimpleAttributeSet set) {
         try {
-            doc.insertString(doc.getLength(), args+"\n", set);
+            doc.insertString(doc.getLength(), args + "\n", set);
         } catch (BadLocationException ex) {
             Logger.getLogger(Exercise.class.getName()).log(Level.SEVERE, null, ex);
         }
-     
+
     }
-    
-    protected void AddTextToPane(String args){
-      SimpleAttributeSet set = new SimpleAttributeSet();
-          try {
-            doc.insertString(doc.getLength(), args+"\n", set);
+
+    protected void AddTextToPane(String args) {
+        SimpleAttributeSet set = new SimpleAttributeSet();
+        try {
+            doc.insertString(doc.getLength(), args + "\n", set);
         } catch (BadLocationException ex) {
             Logger.getLogger(Exercise.class.getName()).log(Level.SEVERE, null, ex);
         }
-     
+
     }
- 
-    
-    protected void buttonAction() {};
- 
+
+    protected void buttonAction() {
+    }
+;
+
 }
